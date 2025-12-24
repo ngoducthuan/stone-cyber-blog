ceaf1ca119bc6e1547e3815ebf5adb427008e09c
ngoducthuan <ngoducthuan24@gmail.com>
2025-12-24 08:30:55 +0700

    fix reload github pages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "App";
 
 // Soft UI Context Provider
-import { SoftUIControllerProvider } from "context";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <SoftUIControllerProvider>
       <App />
     </SoftUIControllerProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
